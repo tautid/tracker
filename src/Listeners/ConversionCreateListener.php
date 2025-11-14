2ab884d7dc389fe84f2c3d541182771216c718e6
SpectreMelody <rudianahadi@gmail.com>
2025-11-14 15:33:42 +0700

    make the listener running on queue

--- src/Listeners/ConversionCreateListener.php
+++ src/Listeners/ConversionCreateListener.php
@@ -2,11 +2,15 @@
 
 namespace TautId\Tracker\Listeners;
 
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
 use TautId\Tracker\Events\ConversionCreateEvent;
 use TautId\Tracker\Factories\PixelTrackerDriverFactory;
 
-class ConversionCreateListener
+class ConversionCreateListener implements ShouldQueue
 {
+    use InteractsWithQueue;
+
     public function handle(ConversionCreateEvent $event): void
     {
         PixelTrackerDriverFactory::getDriver($event->data->pixel->driver)
